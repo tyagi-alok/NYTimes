ed6248e46819215a2dc69fede9d88f96bd8ddb67
Alok Tyagi <alok.tyagi@magicbricks.com>
2020-11-11 12:37:19 +0530

    Added Project in Repository

---
+++ NyTimesApp/NyTimesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NyTimesApp
+//
+//  Created by Alok Tyagi on 11/11/20.
+//  Copyright © 2020 Alok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
